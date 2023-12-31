7f927ebc5f43364f550e9399a1e873040676c446
Guilherme da Silva Benevides <gsbenevides2@gmail.com>
2023-12-31 00:06:39 -0300

    feat: improve editor configuration and log visualization

--- mob_reguler/src/Main.ts
+++ mob_reguler/src/Main.ts
@@ -2,7 +2,15 @@
 import { world } from "@minecraft/server";
 import { calculateIfBlockHasInArea } from "./calculateIfBlockHasInArea";
 
-const mobs = ["creeper", "skeleton", "spider", "zombie", "witch", "enderman"];
+const mobs = [
+  "creeper",
+  "skeleton",
+  "spider",
+  "zombie",
+  "witch",
+  "enderman",
+  "phantom",
+];
 
 const pos1 = {
   x: 1131,
@@ -17,7 +25,7 @@ const pos2 = {
 world.afterEvents.entitySpawn.subscribe((event) => {
   const { entity } = event;
   const { location, dimension, typeId } = entity;
-  const { x, z } = location;
+  const { x, z, y } = location;
   const { id: dimensionId } = dimension;
   const isMob = mobs.some((mob) => typeId.includes(mob));
   const inArea = calculateIfBlockHasInArea({
@@ -25,10 +33,18 @@ world.afterEvents.entitySpawn.subscribe((event) => {
     area: { pos1, pos2 },
   });
   const inOverworld = dimensionId.includes("overworld");
-  console.log("inArea", inArea);
-  console.log("inOverworld", inOverworld, dimensionId);
-  console.log("isMob", isMob, typeId);
-
+  console.log(
+    "Entity Spawned: ",
+    typeId,
+    " at ",
+    x,
+    y,
+    z,
+    " in ",
+    dimensionId,
+    " inArea: ",
+    inArea,
+  );
   if (inArea && inOverworld && isMob) {
     entity.remove();
   }
